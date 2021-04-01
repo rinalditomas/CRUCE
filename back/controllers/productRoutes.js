4eeb1cf76ebaad138d7a1da0a6297c6920de89bc
ivanvera-nvm <veraivandamian@gmail.com>
2021-04-01 11:48:45 -0300

    update database

--- back/controllers/productRoutes.js
+++ back/controllers/productRoutes.js
@@ -1,12 +1,11 @@
 const { Order, Product } = require("../models");
-
 const sequelize = require("sequelize");
-const { count } = require("../models/User");
 
 const productController = {
   async findProductsByOrder(req, res, next) {
+
+    
     const orderNumber = "1119561076530-01";
-    const productSku = "3417";
 
     const products = await Product.findAndCountAll({
       where: { orderNumber },
@@ -18,6 +17,7 @@ const productController = {
     }).catch((e) => console.log(e));
     res.send(products);
   },
+  
 };
 
 module.exports = productController;
